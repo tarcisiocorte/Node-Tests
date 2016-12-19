1a29c086ba89ce4b464fa7421153375a6572b100
Tarcisio Corte <tarcisiocorte@gmail.com>
2016-12-19 23:49:17 +0000

    create a first configuration

---
+++ produtosWeb.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+http.createServer(function(request,response)
+{
+    if(request.url == "/produtos")
+    {
+      response.end("<html><body>listando os produtos da loja</body>")
+    }
+    else
+    {
+      response.end("<html><body>home  da casa do codigo</body></html>");
+    }
+}).listen(3000,"localhost");
+
+console.log("servidor está rodando");
