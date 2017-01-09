595bac93bc227bc24babb9e3bf76567fe00bf06f
Tarcisio Corte <tarcisiocorte@gmail.com>
2017-01-09 21:08:46 +0000

    create the first database connection

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,6 +1,17 @@
 module.exports = function(app){
   //set up the first rout
   app.get('/produtos', function(req,res){
-    res.render("produtos/lista");
+    var mysql = require('mysql');
+    var connection = mysql.createConnection({
+      host :'localhost',
+      user : 'root',
+      password : '',
+      database : 'casadocodigo'
+    });
+
+    connection.query('select * from livros', function(err,results){
+      res.render('produtos/lista',{lista:results});
+    });
+    connection.end();
   });
 }
