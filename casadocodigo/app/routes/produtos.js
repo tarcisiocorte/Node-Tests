5694612376c5bde87f8219897cf067fe60e39c3a
Tarcisio Corte <tarcisiocorte@gmail.com>
2017-01-11 19:02:16 +0000

    adding a factory as connection

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,14 +1,9 @@
+var dbConnection = require('../infra/connectionFacture');
+console.log('connection is working');
 module.exports = function(app){
   //set up the first rout
   app.get('/produtos', function(req,res){
-    var mysql = require('mysql');
-    var connection = mysql.createConnection({
-      host :'localhost',
-      user : 'root',
-      password : '',
-      database : 'casadocodigo'
-    });
-
+    var connection = dbConnection();
     connection.query('select * from livros', function(err,results){
       res.render('produtos/lista',{lista:results});
     });
