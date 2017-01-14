169203b61a3717e21edac0e3e8f4ac49c36d4561
Tarcisio Corte <tarcisiocorte@gmail.com>
2017-01-14 21:28:02 +0000

    The first DAO file has been added.

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,7 +1,7 @@
 module.exports = function(app){
     app.get('/produtos',function(req,res){
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.produtosBanco(connection);
+        var produtosBanco = new app.infra.ProdutosDAO(connection);
         produtosBanco.lista(function(erros,resultados){
             res.render('produtos/lista',{lista:resultados});
             });
@@ -10,7 +10,7 @@ module.exports = function(app){
 
     app.get('produtos/remove',function(){
         var connection = app.infra.connectionFactory();
-        var produtosBanco = app.infra.produtosBanco(connection);
+        var produtosBanco = app.infra.ProdutosDAO(connection);
         var produto = produtosBanco.carrega(id,callback);
         if(produto){
             produtosBanco.remove(produto,callback);
