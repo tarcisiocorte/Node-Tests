4f77871ff56fe666e6046e17e3f7e1879b34fd2c
Tarcisio Corte <tarcisiocorte@gmail.com>
2017-01-14 21:16:38 +0000

    implement callback in the connection

--- casadocodigo/app/infra/produtosBanco.js
+++ casadocodigo/app/infra/produtosBanco.js
@@ -1,6 +1,9 @@
 module.exports = function(){
-        this.lista = function(connection,callback){
-            connection.query('select * from livros',callback);
-            }
-            return this;
+    return function(connection) {
+        this.lista = function(callback){
+            connection.query('select * from livros', callback);
+        }
+        console.log(this);
+        return this;
+    }
 }
--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,12 +1,19 @@
 module.exports = function(app){
     app.get('/produtos',function(req,res){
         var connection = app.infra.connectionFactory();
-        var produtosBanco = app.infra.produtosBanco;
-
-      produtosBanco.lista(connection,function(erros,resultados){
+        var produtosBanco = new app.infra.produtosBanco(connection);
+        produtosBanco.lista(function(erros,resultados){
             res.render('produtos/lista',{lista:resultados});
-
-       });
+            });
         connection.end();
+    });
+
+    app.get('produtos/remove',function(){
+        var connection = app.infra.connectionFactory();
+        var produtosBanco = app.infra.produtosBanco(connection);
+        var produto = produtosBanco.carrega(id,callback);
+        if(produto){
+            produtosBanco.remove(produto,callback);
+        }
     });
 }
