f1845147cf688ef76a2c946edbc473603327831b
Tarcisio Costa Corte de Paula <tarcisiocorte@gmail.com>
2017-01-17 20:11:41 +0000

    fixing the body-parser

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,19 +1,19 @@
 module.exports = function(app){
     app.get('/produtos',function(req,res){
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.ProdutosDAO(connection);
-        produtosBanco.lista(function(erros,resultados){
+        var ProdutosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.lista(function(erros,resultados){
             res.render('produtos/lista',{lista:resultados});
             });
         connection.end();
     });
 
-    app.get('produtos/remove',function(){
-        var connection = app.infra.connectionFactory();
-        var produtosBanco = app.infra.ProdutosDAO(connection);
-        var produto = produtosBanco.carrega(id,callback);
-        if(produto){
-            produtosBanco.remove(produto,callback);
-        }
+    app.get('/produtos/form', function(req, res){
+        res.render('produtos/form');        
+    });
+
+    app.post('/produtos/salva', function(req, res){
+        var produto = req.body;
+        console.log(produto);
     });
 }
