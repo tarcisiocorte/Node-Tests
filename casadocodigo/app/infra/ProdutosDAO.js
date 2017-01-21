f268d67a65f2677b0637c167ea77781257f1ad8e
Tarcisio Corte <tarcisiocorte@gmail.com>
2017-01-21 02:20:19 +0000

    The method to save in the livros table has been created

--- casadocodigo/app/infra/ProdutosDAO.js
+++ casadocodigo/app/infra/ProdutosDAO.js
@@ -6,6 +6,11 @@ ProdutosDAO.prototype.lista = function(callback){
   this._connection.query('select * from livros', callback);
 }
 
+ProdutosDAO.prototype.salva = function(produto,callback){
+  console.log(produto);
+  this._connection.query('insert into livros set ?', produto, callback);
+}
+
 module.exports = function(){
   return ProdutosDAO;
 }
--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,7 +1,7 @@
 module.exports = function(app){
     app.get('/produtos',function(req,res){
         var connection = app.infra.connectionFactory();
-        var ProdutosDAO = new app.infra.ProdutosDAO(connection);
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
         produtosDAO.lista(function(erros,resultados){
             res.render('produtos/lista',{lista:resultados});
             });
@@ -9,11 +9,18 @@ module.exports = function(app){
     });
 
     app.get('/produtos/form', function(req, res){
-        res.render('produtos/form');        
+        res.render('produtos/form');
     });
 
     app.post('/produtos/salva', function(req, res){
-        var produto = req.body;
-        console.log(produto);
+      var produto = req.body;
+
+      var connection = app.infra.connectionFactory();
+      var produtosDAO = new app.infra.ProdutosDAO(connection);
+      produtosDAO.salva(produto, function(erros,resultados){
+        produtosDAO.lista(function(erros,resultados){
+          res.render('produtos/lista',{lista:resultados});
+        });
+      });
     });
 }
