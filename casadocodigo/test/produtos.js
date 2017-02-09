d87972afcecbb00c687e80748fd08eb6f6d66b14
Tarcisio Corte <tarcisiocorte@gmail.com>
2017-02-09 02:07:49 +0000

    Supertest has been added

--- casadocodigo/test/produtos.js
+++ casadocodigo/test/produtos.js
@@ -1,27 +1,12 @@
-var http = require('http');
-describe('#ProdutosController', function(funcaoFinalizacao){
-    it('#listagem json',function(){
-        var configuracoes = {
-        hostname: 'localhost',
-        port:3000,
-        path: '/produtos',
-        headers: {
-            'Accept':'application/json'
-            }
-        };
-    http.get(configuracoes,function(res){
-        if(res.statusCode == 200){
-            console.log("Status ta ok");
-        }
-        if(res.headers['content-type'] == 'application/json;charset=utf-8']);
-            console.log("Content type ok");
+var express = require('../config/express')();
+var request = require('supertest')(express);
 
-        }
-            funcaoFinalizacao();
-    });
-
-        
-        
-    });
+describe('#ProdutosController', function(){
+    it('#listagem json',function(done){
+      request.get('/produtos')
+      .set('Accept','application/json')
+      .expect('Content-Type',/json/)
+      .expect(200,done);
+  });
 
 });
